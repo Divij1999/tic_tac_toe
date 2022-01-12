b1f83363978b04132afa42767292d45ef723ae61
Divij Sumbly <divijsumbly@gmail.com>
2022-01-12 19:57:58 +0530

    Other than the AI which I have not created everything else seems to be working fine. It still looks really bad so I need to make it look better and easier to understand. Next time  I will be working on mainly styling as the aliagnments and appearence is completely messed up.

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ const player= (name, mark) => {
 }
 
 const game=(() => {
-    
-    const _player1= player("first", "X");
-    const _player2= player("second", "O");
 
+    const _player1= player("Player 1", "x");
+        
+    const _player2= player("Player 2", "o");
+
+    const playerNames= (e, name, div) => {
+        
+        
+            const one=document.querySelector(".one");
+            const two=document.querySelector(".two");
+          if(e.target.classList.value==="p1"){
+            _player1.name=name;
+            one.innerText=_player1.name;
+
+          }
+            
+          else {
+            _player2.name=name;
+            two.innerText=_player2.name;
+
+          }
+          div.remove();
+    };
+    
+   
     let _currentPlayer={};
 
     //Toggling the turns between players
-    const togglePlayer= (e) => {
+    const togglePlayer= (e, reset=false) => {
+        if(reset){
+            _currentPlayer={};
+        }
         if(_currentPlayer==={}){
             _currentPlayer=Object.assign({}, _player1);
             console.log(_currentPlayer);
@@ -19,10 +43,10 @@ const game=(() => {
         if(e.target.innerText===""){
 
             if(_currentPlayer.mark===_player1.mark){
-                _currentPlayer=Object.assign({}, _player2);
+                _currentPlayer=Object.assign({},_player2);
             }
             else{
-                _currentPlayer=Object.assign({}, _player1);
+                _currentPlayer=Object.assign({},_player1);
             }
 
         }
@@ -34,49 +58,62 @@ const game=(() => {
     };
 
     const _checkWinner= () => {
+
+        let winner="";
         //Checking horizontally
         for(let i=0; i<=6; i+=3){
-            if(gameBoard.getBoard()[i]==="X" && gameBoard.getBoard()[i+1]==="X" && gameBoard.getBoard()[i+2]==="X"){
+            if(gameBoard.getBoard()[i]==="x" && gameBoard.getBoard()[i+1]==="x" && gameBoard.getBoard()[i+2]==="x"){
                 console.log("x is winner");
+                winner=_player1.name;
             }
-            else if(gameBoard.getBoard()[i]==="O" && gameBoard.getBoard()[i+1]==="O" && gameBoard.getBoard()[i+2]==="O"){
-                console.log("O is winner");
+            else if(gameBoard.getBoard()[i]==="o" && gameBoard.getBoard()[i+1]==="o" && gameBoard.getBoard()[i+2]==="o"){
+                console.log("o is winner");
+                winner=_player2.name;
             }
                   
         }
 
        //Checking vertically
        for(let i=0; i<3; ++i){
-           if(gameBoard.getBoard()[i]==="X" && gameBoard.getBoard()[i+3]==="X" && gameBoard.getBoard()[i+6]==="X"){
+           if(gameBoard.getBoard()[i]==="x" && gameBoard.getBoard()[i+3]==="x" && gameBoard.getBoard()[i+6]==="x"){
                console.log("x is winner");
+               winner=_player1.name;
            }
-           else if(gameBoard.getBoard()[i]==="O" && gameBoard.getBoard()[i+3]==="O" && gameBoard.getBoard()[i+6]==="O"){
-               console.log("O is winner");
+           else if(gameBoard.getBoard()[i]==="o" && gameBoard.getBoard()[i+3]==="o" && gameBoard.getBoard()[i+6]==="o"){
+               console.log("o is winner");
+               winner=_player2.name;
            }
        }
 
        //Checking diagonally
        for(let i=0; i<3; i+=2){
            if(i<2){
-                if(gameBoard.getBoard()[i]==="X" && gameBoard.getBoard()[i+4]==="X" && gameBoard.getBoard()[i+8]==="X"){
+                if(gameBoard.getBoard()[i]==="x" && gameBoard.getBoard()[i+4]==="x" && gameBoard.getBoard()[i+8]==="x"){
                      console.log("x is winner");
+                     winner=_player1.name;
                 }
-                else if(gameBoard.getBoard()[i]==="O" && gameBoard.getBoard()[i+4]==="O" && gameBoard.getBoard()[i+8]==="O"){
-                     console.log("O is winner");
+                else if(gameBoard.getBoard()[i]==="o" && gameBoard.getBoard()[i+4]==="o" && gameBoard.getBoard()[i+8]==="o"){
+                     console.log("o is winner");
+                     winner=_player2.name;
                 }
 
             }
             else if(i===2){
-                if(gameBoard.getBoard()[i]==="X" && gameBoard.getBoard()[i+2]==="X" && gameBoard.getBoard()[i+4]==="X"){
+                if(gameBoard.getBoard()[i]==="x" && gameBoard.getBoard()[i+2]==="x" && gameBoard.getBoard()[i+4]==="x"){
                     console.log("x is winner");
+                    winner=_player1.name;
                }
-               else if(gameBoard.getBoard()[i]==="O" && gameBoard.getBoard()[i+2]==="O" && gameBoard.getBoard()[i+4]==="O"){
-                    console.log("O is winner");
+               else if(gameBoard.getBoard()[i]==="o" && gameBoard.getBoard()[i+2]==="o" && gameBoard.getBoard()[i+4]==="o"){
+                    console.log("o is winner");
+                    winner=_player2.name;
                }
 
             }
        
-    }
+        }
+        if(winner!=="")
+          displayController.displayWinner(winner);
+       
 
     };
 
@@ -88,6 +125,7 @@ const game=(() => {
        togglePlayer,
        getCurrentPlayer,
        getWinner,
+       playerNames,
     };
 })();
 
@@ -115,21 +153,33 @@ const gameBoard= (() => {
         return _board;
     };
 
+    const resetBoard=(e, divs, endScreen) => {
+        for(let i=0; i<_board.length; ++i){
+            _board[i]="";
+        }
+        divs.forEach(div => {div.innerText=_board[0]});
+        game.togglePlayer(e, {});
+        endScreen.remove();
+
+    };
+
     return {
         getBoard,
         getMark,
+        resetBoard,
     };
 })();
 
 
 const displayController= (() => {
+   
     const createBoard= () => {
         const gameBoard=document.querySelector(".gameboard");
         for(let i=0; i<9; ++i){
             const div=document.createElement("div");
             div.classList.value="tile";
             div.setAttribute("data-index", i);
-            div.setAttribute("style", "height:170px; width:170px; border:1px solid black;");
+            div.setAttribute("style", "height:170px; width:170px; border:1px solid black; display:flex; justify-content:center; align-items:center; font-size:12em; font-family:monospace;");
             gameBoard.appendChild(div);
         }
     };
@@ -143,9 +193,64 @@ const displayController= (() => {
     const displaySign= (e, index) => {
         e.target.innerText=gameBoard.getBoard()[index];
     };
+    
+    //Creating input and buttons for name change
+    const changePlayerName= (e) => {
+        const names=document.querySelector(".name");
+       
+        const playerName=document.createElement("input");
+        playerName.classList.value="playerName";
+        
+        const submitBtn=document.createElement("button");
+        submitBtn.classList.value="submit";
+        submitBtn.innerText="Set";
+       
+        const div=document.createElement("div");
+       
+        div.appendChild(playerName);
+        div.appendChild(submitBtn);
+        names.appendChild(div);
+        
+        const event=e;
+        submitBtn.addEventListener("click", (e) => {game.playerNames(event, playerName.value, div)});
+       
+
+    };
+
+    const changeNameOne=document.querySelector(".p1");
+    changeNameOne.addEventListener("click", changePlayerName);
+
+    const changeNameTwo=document.querySelector(".p2");
+    changeNameTwo.addEventListener("click", changePlayerName);
+
+    const displayWinner= (winner) => {
+        const endScreen=document.createElement("div");
+        endScreen.setAttribute("style", "height:100vh; width:100vw; background-color:rgba(255,255,255,1); opacity:0.5; position:fixed; display:flex; justify-content:center; align-items:center;");
+        const endMenu=document.createElement("div");
+        endMenu.setAttribute("style", "display:flex; height:10vh; width:15vw; flex-direction:column; justify-content:space-around;");
+        const results=document.createElement("div");
+        results.innerText=winner;
+       
+        const restartBtn=document.createElement("button");
+        restartBtn.classList.value="restart";
+        restartBtn.innerText="Play Again";
+        restartBtn.addEventListener("click", (e) => {gameBoard.resetBoard(e, divs, endScreen)});
+
+        endMenu.appendChild(results);
+        endMenu.appendChild(restartBtn);
+        endScreen.appendChild(endMenu);
+
+
+        const body=document.querySelector("body");
+        body.appendChild(endScreen);
+
+
+    };
+
 
     return {
         displaySign,
+        displayWinner,
     };
     
 })();
